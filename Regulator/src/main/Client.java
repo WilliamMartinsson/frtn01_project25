fa2295b35e53904f768a4afe64cf65a3eb0ab723
Craphtex <christian@crapht.se>
2013-12-03 14:06:06 +0100

    Debug

--- Regulator/src/main/Client.java
+++ Regulator/src/main/Client.java
@@ -5,16 +5,21 @@ import regulator.Regul;
 import util.IO;
 
 public class Client {
-
-	public static void main(String[] args) {
+	
+	public Client(){
 		IO angle = IO.getIO(0);
 		IO pos = IO.getIO(1);
 		IO signal = IO.getIO(2);
 		signal.setValue(-10);
 		Regul regul = new Regul(0, angle, pos, signal);
 		TwoWaySerialComm comm = new TwoWaySerialComm(new String[] {}, angle,
-				pos, signal);
+				pos, signal,"/dev/ttyUSB0",57600,1024);
+		comm.start();
 		regul.start();
+	}
+
+	public static void main(String[] args) {
+		new Client();
 	}
 
 }
