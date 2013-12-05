e84486a329ca432e99fb6832795a108d78e0fd5d
WilliamMartinsson <dt09wm2@heron-12.control.lth.se>
2013-12-05 14:31:47 +0100

    Small refactor

--- Regulator/src/main/Client.java
+++ Regulator/src/main/Client.java
@@ -13,62 +13,69 @@ import webmonitor.WebMonitor;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class Client {
-	
-	private SocketMonitor socketMonitor;
-	
-	public Client(){
+public class Client extends Thread {
+
+	private SocketMonitor socketMonitor = null;
+	private WebMonitor webMonitor = null;
+	Regul regul = null;
+	TwoWaySerialComm comm = null;
+
+	public Client() {
 		IOMonitor angle = IOMonitor.getIO(IOMonitor.ANGLE);
 		IOMonitor pos = IOMonitor.getIO(IOMonitor.POSITION);
 		IOMonitor y = IOMonitor.getIO(IOMonitor.Y);
 
-		SocketMonitor socketMonitor = null;
 		try {
-			RegulatorSocket rs = new RegulatorSocket(Main.REGULATOR_PORT,Main.REGULATOR_HOST);
+			RegulatorSocket rs = new RegulatorSocket(Main.REGULATOR_PORT,
+					Main.REGULATOR_HOST);
 			socketMonitor = rs.getMonitor();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-        long time = System.currentTimeMillis();
-        WebMonitor webMonitor = new WebMonitor(Main.WEBMONITOR_HOST);
-        HashMap<String, Double> constants = webMonitor.getConfiguration();
-        Util.print("[GET] Constants: " + (System.currentTimeMillis() - time) + "ms");
+		long time = System.currentTimeMillis();
+		webMonitor = new WebMonitor(Main.WEBMONITOR_HOST);
+		HashMap<String, Double> constants = webMonitor.getConfiguration();
+		Util.print("[GET] Constants: " + (System.currentTimeMillis() - time)
+				+ "ms");
 
+		regul = new Regul(0, angle, pos, y, webMonitor);
 
-		Regul regul = new Regul(0, angle, pos, y, webMonitor);
+		// WARNING: If these values are **** then the process will be ****
+		// PIParameters piParameters = new PIParameters();
+		// piParameters.K = constants.get("k");
+		// piParameters.Ti = constants.get("ti");
+		// piParameters.Tr = constants.get("tr");
+		// piParameters.Beta = constants.get("beta");
+		// piParameters.H = constants.get("h");
+		// piParameters.integratorOn = false;
+		// regul.setInnerParameters(piParameters);
 
-        // WARNING:  If these values are **** then the process will be ****
-//        PIParameters piParameters = new PIParameters();
-//        piParameters.K    = constants.get("k");
-//        piParameters.Ti   = constants.get("ti");
-//        piParameters.Tr   = constants.get("tr");
-//        piParameters.Beta = constants.get("beta");
-//        piParameters.H    = constants.get("h");
-//        piParameters.integratorOn = false;
-//        regul.setInnerParameters(piParameters);
-
-
-//        PIDParameters pidParameters = new PIDParameters();
-//        pidParameters = new PIDParameters();
-//        pidParameters.Beta = constants.get("beta");
-//        pidParameters.H = constants.get("h");
-//        pidParameters.integratorOn = false;
-//        pidParameters.K = constants.get("k");
-//        pidParameters.Ti = constants.get("ti");
-//        pidParameters.Tr = constants.get("tr");
-//        pidParameters.Td = constants.get("td");
-//        pidParameters.N = constants.get("n");
-//        regul.setOuterParameters(pidParameters);
+		// PIDParameters pidParameters = new PIDParameters();
+		// pidParameters = new PIDParameters();
+		// pidParameters.Beta = constants.get("beta");
+		// pidParameters.H = constants.get("h");
+		// pidParameters.integratorOn = false;
+		// pidParameters.K = constants.get("k");
+		// pidParameters.Ti = constants.get("ti");
+		// pidParameters.Tr = constants.get("tr");
+		// pidParameters.Td = constants.get("td");
+		// pidParameters.N = constants.get("n");
+		// regul.setOuterParameters(pidParameters);
 
 		regul.setBALLMode();
 
-
-		TwoWaySerialComm comm = new TwoWaySerialComm(new String[] {}, angle,
-				pos, y,"/dev/ttyUSB0",57600,1024);
+		comm = new TwoWaySerialComm(new String[] {}, angle, pos, y,
+				"/dev/ttyUSB0", 57600, 1024);
 		comm.start();
 		regul.start();
+	}
+
+	public void run() {
+		while (!Thread.interrupted()) {
+			
+		}
 	}
 
 	public static void main(String[] args) {
